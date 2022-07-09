0d0f4badf2a0f75df105e27d432f367378660f1b
vc-souza <vinicius.cp.souza@gmail.com>
2022-07-09 16:54:10 -0300

    Better docs

--- algo/doc.go
+++ algo/doc.go
@@ -1,2 +1,2 @@
-// Package algo implements graph algorithms using the data structures from the 'ds' package.
+// Package algo implements graph algorithms using the data structures from the 'ds' package. Any assertions regarding asymptotic behavior assume the worst-case scenario.
 package algo
