959061e2e482c783b312e33b40ea2d266948f49f
vc-souza <vinicius.cp.souza@gmail.com>
2022-07-12 09:40:32 -0300

    Updating .gitignore, adding main files

---
+++ samples/bfs/main.go
@@ -0,0 +1,155 @@
+//go:build !test
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/vc-souza/gga/algo"
+	"github.com/vc-souza/gga/ds"
+	"github.com/vc-souza/gga/viz"
+)
+
+const (
+	bfsFileIn  = "BFS-in.dot"
+	bfsFileOut = "BFS-out.dot"
+)
+
+var (
+	defaultGraphFmt = ds.FmtAttrs{
+		"layout": "dot",
+	}
+
+	defaultVertexFmt = ds.FmtAttrs{
+		"shape": "circle",
+		"style": "filled",
+	}
+
+	defaultEdgeFmt = ds.FmtAttrs{
+		"pencolor": "#000000",
+	}
+)
+
+func buildBFSInput() (*ds.Graph[ds.Text], *ds.Text) {
+	a := ds.Text("A")
+	r := ds.Text("R")
+	s := ds.Text("S")
+	t := ds.Text("T")
+	u := ds.Text("U")
+	v := ds.Text("V")
+	w := ds.Text("W")
+	x := ds.Text("X")
+	y := ds.Text("Y")
+
+	g := ds.NewUndirectedGraph[ds.Text]()
+
+	g.AddVertex(&a)
+
+	g.AddUnweightedEdge(&r, &s)
+	g.AddUnweightedEdge(&r, &v)
+
+	g.AddUnweightedEdge(&s, &r)
+	g.AddUnweightedEdge(&s, &w)
+
+	g.AddUnweightedEdge(&t, &u)
+	g.AddUnweightedEdge(&t, &w)
+	g.AddUnweightedEdge(&t, &x)
+
+	g.AddUnweightedEdge(&u, &t)
+	g.AddUnweightedEdge(&u, &x)
+	g.AddUnweightedEdge(&u, &y)
+
+	g.AddUnweightedEdge(&v, &r)
+
+	g.AddUnweightedEdge(&w, &s)
+	g.AddUnweightedEdge(&w, &t)
+	g.AddUnweightedEdge(&w, &x)
+
+	g.AddUnweightedEdge(&x, &t)
+	g.AddUnweightedEdge(&x, &u)
+	g.AddUnweightedEdge(&x, &w)
+	g.AddUnweightedEdge(&x, &y)
+
+	g.AddUnweightedEdge(&y, &u)
+	g.AddUnweightedEdge(&y, &x)
+
+	return g, &s
+}
+
+func onBFSTreeVertex(v *ds.GraphVertex[ds.Text], n *algo.BFSNode[ds.Text]) {
+	v.SetFmtAttr("label", fmt.Sprintf(`%s\nd=%d`, v.Label(), int(n.Distance)))
+	v.SetFmtAttr("fillcolor", "#000000")
+	v.SetFmtAttr("fontcolor", "#ffffff")
+	v.SetFmtAttr("shape", "doublecircle")
+}
+
+func onBFSUnVertex(v *ds.GraphVertex[ds.Text], n *algo.BFSNode[ds.Text]) {
+	v.SetFmtAttr("fillcolor", "#ff0000")
+	v.SetFmtAttr("fontcolor", "#ffffff")
+}
+
+func onBFSSourceVertex(v *ds.GraphVertex[ds.Text], n *algo.BFSNode[ds.Text]) {
+	v.SetFmtAttr("shape", "circle")
+	v.SetFmtAttr("pos", "0,0!")
+}
+
+func onBFSTreeEdge(e *ds.GraphEdge[ds.Text]) {
+	e.SetFmtAttr("penwidth", "3.0")
+}
+
+func main() {
+	// build graph, establish a source vertex
+	g, src := buildBFSInput()
+	ex := viz.NewExporter(g)
+
+	ex.DefaultGraphFmt = defaultGraphFmt
+	ex.DefaultVertexFmt = defaultVertexFmt
+	ex.DefaultEdgeFmt = defaultEdgeFmt
+
+	fIn, err := os.Create(bfsFileIn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer fIn.Close()
+
+	// export the input graph
+	g.Accept(ex)
+	ex.Export(fIn)
+
+	// run BFS with the given source
+	tree, err := algo.BFS(g, src)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fOut, err := os.Create(bfsFileOut)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer fOut.Close()
+
+	vi := viz.NewBFSViz(g, tree, src)
+
+	// set the desired custom formatting
+	vi.DefaultGraphFmt = defaultGraphFmt
+	vi.DefaultVertexFmt = defaultVertexFmt
+	vi.DefaultEdgeFmt = defaultEdgeFmt
+	vi.OnTreeVertex = onBFSTreeVertex
+	vi.OnSourceVertex = onBFSSourceVertex
+	vi.OnUnVertex = onBFSUnVertex
+	vi.OnTreeEdge = onBFSTreeEdge
+
+	// annotate the input graph with the result of the BFS,
+	// then export the annotated version
+	err = vi.Export(fOut)
+
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ samples/text/main.go
@@ -0,0 +1,71 @@
+//go:build !test
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/vc-souza/gga/ds"
+	"github.com/vc-souza/gga/viz"
+)
+
+var undirectedInput = `
+graph
+a#b:4,h:8
+b#a:4,c:8,h:11
+c#b:8,d:7,i:2,f:4
+d#c:7,e:9,f:14
+e#d:9,f:10
+f#c:4,d:14,e:10,g:2
+g#f:2,h:1,i:6
+h#a:8,b:11,g:1,i:7
+i#c:2,g:6,h:7
+j#
+`
+
+var directedInput = `
+digraph
+1#2,4
+2#5
+3#5,6
+4#2
+5#4
+6#6
+7#
+`
+
+func export(src string) {
+	g, err := ds.ParseGraph(src)
+	ex := viz.NewExporter(g)
+
+	if err != nil {
+		panic(err)
+	}
+
+	g.Accept(ex)
+	ex.Export(os.Stdout)
+}
+
+func main() {
+	var opt string
+
+	fmt.Println("Pick a graph type:")
+	fmt.Println("1. Digraph")
+	fmt.Println("2. Graph")
+
+	_, err := fmt.Scanln(&opt)
+
+	if err != nil {
+		panic(err)
+	}
+
+	switch opt {
+	case "1":
+		export(directedInput)
+	case "2":
+		export(undirectedInput)
+	default:
+		panic("invalid choice!")
+	}
+}
