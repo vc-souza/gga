9bd56f43ddefb03b1188988d99bd1fa99283ca7a
vc-souza <vinicius.cp.souza@gmail.com>
2022-07-15 00:10:29 -0300

    More docs

--- viz/shared.go
+++ viz/shared.go
@@ -6,7 +6,10 @@ import (
 	"github.com/vc-souza/gga/ds"
 )
 
-// TODO: docs
+/*
+ThemedGraphViz contains data that is useful for graph algorithm visualizations.
+When embedded, it also provides a good part of the AlgoViz interface for free.
+*/
 type ThemedGraphViz[V ds.Item] struct {
 	Graph *ds.Graph[V]
 	Theme Theme
@@ -20,14 +23,17 @@ func (v *ThemedGraphViz[V]) GetTheme() Theme {
 	return v.Theme
 }
 
-// TODO: docs
+/*
+An AlgoViz implementer can traverse the results of a graph algorithm,
+provide its input graph, and also support theming.
+*/
 type AlgoViz[V ds.Item] interface {
 	GetGraph() *ds.Graph[V]
 	GetTheme() Theme
 	Traverse() error
 }
 
-// TODO: docs
+// ExportViz can guide the execution of an AlgoViz implementation and then export its results.
 func ExportViz[V ds.Item](vi AlgoViz[V], w io.Writer) error {
 	ex := NewExporter(vi.GetGraph())
 
