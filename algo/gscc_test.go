23197a66647a088e71e24786879a55e3dad5823e
vc-souza <vinicius.cp.souza@gmail.com>
2022-07-17 17:57:13 -0300

    Adding tests

---
+++ algo/gscc_test.go
@@ -0,0 +1,56 @@
+package algo
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/vc-souza/gga/ds"
+	ut "github.com/vc-souza/gga/internal/testutils"
+)
+
+func itemGroupTag(ig *ds.ItemGroup[ds.Text]) string {
+	s := make([]string, 0, len(ig.Items))
+
+	for _, item := range ig.Items {
+		s = append(s, item.Label())
+	}
+
+	return strings.Join(s, ",")
+}
+
+func TestGSCC_directed(t *testing.T) {
+	expect := []string{
+		"w,v,s",
+		"z,x",
+		"y,t,q",
+		"u",
+		"r",
+	}
+
+	g, _, err := ds.NewTextParser().Parse(ut.UDGDeps)
+
+	ut.Equal(t, true, err == nil)
+
+	gscc, _, err := GSCC(g)
+
+	ut.Equal(t, true, err == nil)
+
+	ut.Equal(t, 5, gscc.VertexCount())
+	ut.Equal(t, 5, gscc.EdgeCount())
+
+	for i := range expect {
+		ut.Equal(t, expect[i], itemGroupTag(gscc.Verts[i].Val))
+	}
+}
+
+func TestGSCC_undirected(t *testing.T) {
+	g, _, err := ds.NewTextParser().Parse(ut.UUGSimple)
+
+	ut.Equal(t, true, err == nil)
+
+	_, _, err = GSCC(g)
+
+	ut.Equal(t, true, err != nil)
+	ut.Equal(t, true, errors.Is(err, ds.ErrUndefOp))
+}
---
+++ viz/gscc_test.go
@@ -0,0 +1,31 @@
+package viz
+
+import (
+	"testing"
+
+	"github.com/vc-souza/gga/algo"
+	"github.com/vc-souza/gga/ds"
+	ut "github.com/vc-souza/gga/internal/testutils"
+)
+
+func TestGSCCViz(t *testing.T) {
+	g, _, err := ds.NewTextParser().Parse(ut.UDGDeps)
+
+	ut.Equal(t, true, err == nil)
+
+	gscc, _, err := algo.GSCC(g)
+
+	ut.Equal(t, true, err == nil)
+
+	vi := NewGSCCViz(gscc, nil)
+
+	vCount := 0
+
+	vi.OnGSCCVertex = func(v *ds.GraphVertex[ds.ItemGroup[ds.Text]]) {
+		vCount++
+	}
+
+	ExportViz[ds.ItemGroup[ds.Text]](vi, ut.DummyWriter{})
+
+	ut.Equal(t, gscc.VertexCount(), vCount)
+}
