c3f0ba02dbc6a9d9e9056571559fd1f066d33c65
vc-souza <vinicius.cp.souza@gmail.com>
2022-07-17 17:38:16 -0300

    Algo docs

--- algo/gscc.go
+++ algo/gscc.go
@@ -6,7 +6,34 @@ import (
 	"github.com/vc-souza/gga/ds"
 )
 
-// TODO: docs
+/*
+GSCC implements an algorithm for building a condensation graph GSCC from a directed graph.
+
+A condensation graph is the result of contracting each strongly connected component
+(SCC) of the original graph to a vertex in the GSCC. Each edge (u, v) in the GSCC
+exists because there exists at least one edge (x, y) in the original graph such that
+x is in the SCC u and y is in the SCC v. One important property of the condensation
+graph GSCC is that it is guaranteed to be a DAG (Directed Acyclic Graph).
+
+Tarjan's SCC algorithm is used to calculate the SCCs because of its side-effect
+of returning the SCCs in reverse topological order of the GSCC, which means
+that if the SCC list is traversed in reverse we will never examine an edge
+connecting the current SCC to an SCC that was examined earlier. In theory,
+the space required to keep track of possible adjacencies between SCCs
+is reduced by one after each SCC that is examined, which would not change
+the worst-case space complexity of this step, O(V), but might end up yielding
+better constant factors in the end.
+
+Link: https://en.wikipedia.org/wiki/Strongly_connected_component#Definitions
+
+Expectations:
+	- The graph is correctly built.
+	- The graph is directed.
+
+Complexity:
+	- Time:  Θ(V + E)
+	- Space: Θ(V)
+*/
 func GSCC[V ds.Item](g *ds.Graph[V]) (*ds.Graph[ds.ItemGroup[V]], []SCC[V], error) {
 	if g.Undirected() {
 		return nil, nil, ds.ErrUndefOp
@@ -19,7 +46,10 @@ func GSCC[V ds.Item](g *ds.Graph[V]) (*ds.Graph[ds.ItemGroup[V]], []SCC[V], erro
 		return nil, nil, err
 	}
 
-	// TODO: explain, Θ(V) space
+	// Using Θ(V) extra space to achieve O(1) amortized
+	// query time later, when the SCC that a vertex
+	// belongs to will need to be queried Θ(E) times,
+	// when building the adjacency list of the GSCC.
 	vtxSCC := make(map[*V]int)
 
 	for id, scc := range sccs {
@@ -28,10 +58,13 @@ func GSCC[V ds.Item](g *ds.Graph[V]) (*ds.Graph[ds.ItemGroup[V]], []SCC[V], erro
 		}
 	}
 
-	// TODO: explain
 	gscc := ds.NewDirectedGraph[ds.ItemGroup[V]]()
 
-	// TODO: explain (cc id == vtx id int he GSCC)
+	// By aligning the SCC id with the id of their
+	// vertex in the GSCC we can get the ItemGroup
+	// in O(1) amortized time later, without having
+	// to use any extra space to map SCC ids to their
+	// respective ItemGroup.
 	for id := range sccs {
 		gscc.UnsafeAddVertex(
 			&ds.ItemGroup[V]{
@@ -41,44 +74,60 @@ func GSCC[V ds.Item](g *ds.Graph[V]) (*ds.Graph[ds.ItemGroup[V]], []SCC[V], erro
 		)
 	}
 
-	// TODO: explain, O(V) space
+	// Using O(V) space to keep trap of the adjacency
+	// relationships between the SCCs in the GSCC.
 	gsccAdj := make([]int, len(sccs)-1)
 
-	// TODO: explain (also, why not 0?)
+	// Traversing the SCC list in reverse to capitalize
+	// on the fact that Tarjan's SCC algorithm returns
+	// the list in reverse topological order of the GSCC.
+	// Using this property, the SCC with index 0 can be
+	// skipped, since it is the last one in that order.
 	for srcId := len(sccs) - 1; srcId > 0; srcId-- {
-		// TODO: explain
 		for _, v := range sccs[srcId] {
 			for _, e := range g.Adj[v] {
 				dstId := vtxSCC[e.Dst]
 
-				// same SCC, bail
+				// vertices in the same SCC, skip.
 				if srcId == dstId {
 					continue
 				}
 
-				// TODO: explain
-				// edge already exists, bail
+				// Since the same slice is used by all components
+				// to keep track of their adjacencies, and we
+				// do not want to zero the whole thing after
+				// each SCC is processed, we are using the id
+				// of the current SCC to keep track of edges.
+				// Which means that, while looking at a position
+				// in the slice, if any other number that is not
+				// the id of the current SCC is found, we treat
+				// it as a 0, and the respective edge as not
+				// currently existing.
+
+				// edge between SCCs already exists, skip.
 				if gsccAdj[dstId] == srcId {
 					continue
 				}
 
-				// TODO: explain: topo sort,
-				// one edges now are going forwards
-
-				// TODO: explain
+				// Since the SCC list and the GSCC vertex list are aligned,
+				// we can find the ItemGroup assigned to SCC x by looking
+				// at the vertex of GSCC at index x.
 				gscc.UnsafeAddWeightedEdge(
 					gscc.Verts[srcId].Val,
 					gscc.Verts[dstId].Val,
 					0,
 				)
 
-				// TODO: explain
+				// marking the edge for the current SCC
 				gsccAdj[dstId] = srcId
 			}
 		}
 
-		// TODO: explain
-		gsccAdj = gsccAdj[:srcId-1]
+		// Attempting to capitalize on the property that there will
+		// never be any edges in the next iteration that arrive at
+		// vertices contained in SCCs already examined, by shrinking
+		// the slice being used to keep track of adjacencies.
+		gsccAdj = gsccAdj[: srcId-1 : srcId]
 	}
 
 	return gscc, sccs, nil
