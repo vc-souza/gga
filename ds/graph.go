8874b1b93448a7fa7157d99d457e19481ffc0dde
vc-souza <vinicius.cp.souza@gmail.com>
2022-07-24 13:01:34 -0300

    Adding graph docs

--- ds/graph.go
+++ ds/graph.go
@@ -5,14 +5,29 @@ import (
 	"strings"
 )
 
-// TODO: docs
+/*
+A GE represents a connection between two vertices in a graph, with an optional weight.
+The directed/undirected nature of the edge is given by the graph that owns it.
+*/
 type GE struct {
 	Formattable
 
+	/*
+		Src is the id of the vertex at the source / tail of the edge.
+		This edge contributes to the out-degree of that vertex.
+	*/
 	Src int
-	Dst int
-	Wt  float64
 
+	/*
+		Dst is the id of the vertex at the destination / head of the edge.
+		This edge contributes to the in-degree of that vertex.
+	*/
+	Dst int
+
+	// Wt is the weight/cost associated with the edge.
+	Wt float64
+
+	// Index is the position of this edge in Src's adjacency list.
 	Index int
 }
 
@@ -26,13 +41,18 @@ func (e *GE) String() string {
 	)
 }
 
-// TODO: docs
+// A GV represents a vertex in a graph.
 type GV struct {
 	Formattable
 
+	// Item is the satellite data of the vertex, coming along with it wherever it goes.
+	Item Item
+
+	// Index is the index of the vertex in the list of vertices of the graph.
 	Index int
-	Item  Item
-	E     []GE
+
+	// E is the adjacency list for the vertex, listing all edges that the vertex as their source.
+	E []GE
 }
 
 func (v *GV) Label() string {
@@ -55,7 +75,13 @@ func (v *GV) String() string {
 	return b.String()
 }
 
-// TODO: docs
+/*
+G implements a directed or undirected graph G = (V, E), using adjacency lists
+to achieve linear space complexity: Θ(V + E). Due to the nature of adjacency lists,
+checking whether an edge (u, v) exists has O(E) time complexity. For applications
+that make heavy use of this operation, an adjacency matrix would be a better fit
+(O(1) time complexity), with the trade-off being a worse space complexity of Θ(V²).
+*/
 type G struct {
 	V []GV
 
@@ -65,7 +91,6 @@ type G struct {
 	vCount int
 }
 
-// TODO: docs
 func newG(dir bool) *G {
 	g := &G{}
 
@@ -77,12 +102,12 @@ func newG(dir bool) *G {
 	return g
 }
 
-// TODO: docs
+// NewGraph creates a new undirected graph.
 func NewGraph() *G {
 	return newG(false)
 }
 
-// TODO: docs
+// NewDigraph creates a new directed graph.
 func NewDigraph() *G {
 	return newG(true)
 }
